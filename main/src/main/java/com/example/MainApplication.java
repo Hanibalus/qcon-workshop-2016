eb54233acc5f4a5699654fee8cf2b9319983eac6
Dave Syer <dsyer@pivotal.io>
2016-03-10 15:19:51 +0000

    Add /user endpoint to show the data coming from Github

--- main/src/main/java/com/example/MainApplication.java
+++ main/src/main/java/com/example/MainApplication.java
@@ -1,16 +1,26 @@
 package com.example;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableZuulProxy
 @EnableOAuth2Sso
+@RestController
 public class MainApplication {
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);

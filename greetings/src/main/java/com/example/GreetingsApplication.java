ca4ebd49e8f7da8b059f6aacef8f1c5c581e5399
Dave Syer <dsyer@pivotal.io>
2016-03-10 13:29:59 +0000

    Rip out Eureka completely and use Ribbon server list

--- greetings/src/main/java/com/example/GreetingsApplication.java
+++ greetings/src/main/java/com/example/GreetingsApplication.java
@@ -8,12 +8,10 @@ import javax.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GreetingsApplication {
 	
 	@Autowired
